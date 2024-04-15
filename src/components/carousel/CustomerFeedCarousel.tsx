393f1923aa59fc83e2fa04444a84aea427b2e05e
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-04-15 17:22:58 +0530

    chore: feedback card issue fix

--- src/components/carousel/CustomerFeedCarousel.tsx
+++ src/components/carousel/CustomerFeedCarousel.tsx
@@ -13,8 +13,8 @@ export default function CustomerFeedCarousel({
   const end = start + slides;
   return (
     <>
-      <div className=' flex flex-col justify-center items-center mb-8   '>
-        <div className='flex  gap-7 mt-16 mb-8 w-[90%] lg:w-[88%] xl:w-[85%]  justify-center'>
+      <div className=' flex flex-col justify-center items-center mb-8    '>
+        <div className='flex  gap-7 mt-16 mb-8 sm:w-[100%] md:w-[90%] lg:w-[88%] xl:w-[85%]  justify-center'>
           {testimonials.slice(start, end).map((item) => (
             <TestimonialCard
               key={item.customer}
