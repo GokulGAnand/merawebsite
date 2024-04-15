fdcb58b2ef5206603507585c4001e417ea7a8db7
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-04-15 11:47:04 +0530

    feat: add daisy header

--- src/components/links/HamburgerMenu.tsx
+++ src/components/links/HamburgerMenu.tsx
@@ -1,19 +1,49 @@
-'use client';
-import Sidebar from '@/components/layout/Sidebar';
-import React, { useState } from 'react';
+import { headerLinks } from '@/data/header-links';
+import Link from 'next/link';
 import { IoMdMenu } from 'react-icons/io';
+import { IoCloseOutline } from 'react-icons/io5';
+import Logo from '~/images/logoHeader.svg';
+import { SidebarLinks } from './SidebarLinks';
 
 export default function HamburgerMenu() {
-  const [show, setShow] = useState(false);
-  function handleToggle() {
-    setShow(!show);
-  }
   return (
-    <>
-      <div onClick={handleToggle} className='cursor-pointer flex lg:hidden'>
-        <IoMdMenu className='w-6 h-5' />
+    <div className='drawer lg:hidden flex'>
+      <input id='my-drawer' type='checkbox' className='drawer-toggle' />
+      <div className='drawer-content'>
+        {/* Icon  */}
+        <label htmlFor='my-drawer' className='cursor-pointer flex lg:hidden'>
+          <IoMdMenu className='w-6 h-5' />
+        </label>
       </div>
-      <Sidebar show={show} toggle={handleToggle} />
-    </>
+      <div className='drawer-side'>
+        <label
+          htmlFor='my-drawer'
+          aria-label='close sidebar'
+          className='drawer-overlay'
+        ></label>
+        <ul className='menu p-0 min-h-full w-64 min-w-fit z-0 bg-white flex flex-col justify-between'>
+          <div>
+            <div className='flex w-full justify-between py-5 px-2  shadow-md '>
+              <div className='flex align-middle place-items-center '>
+                <Link href='/'>
+                  <div className='cursor-pointer '>
+                    <Logo width={144} height={32} className='w-36 h-8' />
+                  </div>
+                </Link>
+              </div>
+              <div className='flex align-middle place-items-center'>
+                <label htmlFor='my-drawer'>
+                  <IoCloseOutline fontSize={28} />
+                </label>
+              </div>
+            </div>
+            <SidebarLinks />
+          </div>
+          <div className='bg-bannerChipColor h-[11vh] flex text-center font-bold justify-center align-middle place-items-center '>
+            Call us at 9999999999
+          </div>
+        </ul>
+      </div>
+    </div>
   );
 }
---
+++ src/components/links/SidebarLinks.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { headerLinks } from '@/data/header-links';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+export const SidebarLinks = () => {
+  const path = usePathname();
+  return (
+    <div>
+      {headerLinks.map((link) => {
+        const isActive = link.route === path;
+        return (
+          <div
+            key={link.label}
+            className={` px-5 py-4 border border-solid border-b-mobileSidebarBorder border-x-0 border-t-0  text-${
+              isActive ? 'bannerChipText' : `headerLinkColor`
+            }`}
+          >
+            {link.label}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
