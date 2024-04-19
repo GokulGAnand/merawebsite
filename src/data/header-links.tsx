fb6bf6a4f9fa3c863596507dab3a44749ea5d35a
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-04-19 18:04:07 +0530

    chore: hide header links

--- src/data/header-links.tsx
+++ src/data/header-links.tsx
@@ -1,18 +1,18 @@
 export const headerLinks = [
-  {
-    label: 'Home',
-    route: '/',
-  },
-  {
-    label: 'Sell Your Car',
-    route: '/sell',
-  },
-  {
-    label: 'MeraPartners',
-    route: '/mera',
-  },
-  {
-    label: 'Car Inspection',
-    route: '/car',
-  },
+  // {
+  //   label: 'Home',
+  //   route: '/',
+  // },
+  // {
+  //   label: 'Sell Your Car',
+  //   route: '/sell',
+  // },
+  // {
+  //   label: 'MeraPartners',
+  //   route: '/mera',
+  // },
+  // {
+  //   label: 'Car Inspection',
+  //   route: '/car',
+  // },
 ];
