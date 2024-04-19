baebd89f8a2da0d3dc6570b55ca76faeba7c600f
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-04-19 16:15:42 +0530

    feat: refund

--- src/data/refund.tsx
+++ src/data/refund.tsx
@@ -5,23 +5,23 @@ export const refund = [
     content: `Last Updated: 12-04-2024
     <br/>  Thank you for choosing Meracars as your trusted platform for buying cars! <br/> <br/>
 
-    1. Refund Eligibility <br/>
+    <strong>1. Refund Eligibility</strong>  <br/>
 Car dealers who have paid the Security deposit of 10,000 INR are eligible for a refund if they are dissatisfied with our platform. To qualify for a refund, please ensure that: <br/> <br/>
 
 •You request a refund within 30 days of your onboarding fee payment.  <br/>
 •You have not actively engaged in any auctions or transactions on the platform. <br/>
 •Your account is in good standing with no violations of our terms of service. <br/> <br/>
 
-2. Refund Process <br/>
+<strong>2. Refund Process</strong>  <br/>
 To request a refund, please contact our customer service team at office@teamtechmedia.com with your account details and the reason for the refund request. Our team will review your request and initiate the refund process if you meet the eligibility criteria.
 <br/> <br/>
-3. Refund Amount <br/>
+<strong>3. Refund Amount</strong> <br/>
 Once your request is approved, we'll promptly refund the entire 10,000 INR to your original payment method. Please allow 5-7 business days for the refund to be processed and credited back to your account.
 <br/> <br/>
-4. Exclusions <br/>
+<strong>4. Exclusions</strong> <br/>
 Please note that once you have participated in any auctions or transactions on our platform, you are no longer eligible for a refund of the security deposit.
 <br/> <br/>
-5. Contact Us <br/>
+<strong>5. Contact Us</strong> <br/>
 If you have any questions or concerns about our refund policy, please don't hesitate to contact us at office@teamtechmedia.com
 <br/> <br/>
 
