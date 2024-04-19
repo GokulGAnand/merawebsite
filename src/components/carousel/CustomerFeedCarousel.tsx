cf13326fb1c29fba46633fff104ad47715fd191d
itssalini <salini@teamtech.com>
2024-04-19 17:18:50 +0530

    style: fix the responsiveness of cards

--- src/components/carousel/CustomerFeedCarousel.tsx
+++ src/components/carousel/CustomerFeedCarousel.tsx
@@ -14,7 +14,7 @@ export default function CustomerFeedCarousel({
   return (
     <>
       <div className=' flex flex-col justify-center items-center mb-8    '>
-        <div className='flex  gap-7 mt-16 mb-8 sm:w-[100%] md:w-[90%] lg:w-[88%] xl:w-[85%]  justify-center'>
+        <div className='flex  gap-7 mt-16 mb-8 w-[100%]  lg:w-[88%] xl:w-[85%]  justify-center'>
           {testimonials.slice(start, end).map((item) => (
             <TestimonialCard
               key={item.customer}
