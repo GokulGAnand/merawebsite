e6f2e7fe7871e7d5c880f1299de356ad26efa8a3
itssalini <salini@teamtech.com>
2024-04-20 10:59:13 +0530

    style: change testimonial images

--- src/data/testimonials.tsx
+++ src/data/testimonials.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import CustomerOne from '~/svg/CustomerOne.svg';
-import CustomerTwo from '~/svg/CustomerTwo.svg';
-import CustomerThree from '~/svg/CustomerThree.svg';
+import Vyshak from '~/svg/Vyshak.svg';
+import Jamshad from '~/svg/Jamshad.svg';
+import Derin from '~/svg/Derin.svg';
+import Nighil from '~/svg/Nighil.svg';
+
 export const testimonials = [
   {
-    icon: <CustomerOne width={60} height={60} />,
-    rating: 5,
+    icon: <Vyshak width={60} height={60} />,
+    rating: 2.5,
     customer: 'Vyshak',
     comments:
       'Selling my car was super quick and easy. Got a great deal without any trouble!',
   },
   {
-    icon: <CustomerTwo width={60} height={60} />,
+    icon: <Jamshad width={60} height={60} />,
     rating: 4,
     customer: 'Jamshad',
     comments:
       'The inspection at home was so convenient. Really happy with the honest service.',
   },
   {
-    icon: <CustomerThree width={60} height={60} />,
+    icon: <Derin width={60} height={60} />,
     rating: 4,
     customer: 'Derin',
     comments:
       'I received my payment the very next day! Completely satisfied with the fast process.',
   },
   {
-    icon: <CustomerTwo width={60} height={60} />,
+    icon: <Nighil width={60} height={60} />,
     rating: 4,
     customer: 'Nighil',
     comments:
