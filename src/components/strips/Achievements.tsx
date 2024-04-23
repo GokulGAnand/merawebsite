eabe9376d220217f64b42995ac6f1d9d6e49e4fe
itssalini <salini@teamtech.com>
2024-04-23 21:24:41 +0530

    feat: achievements on about page

--- src/components/strips/Achievements.tsx
+++ src/components/strips/Achievements.tsx
@@ -1,13 +1,20 @@
+import { achievements } from '@/data/achievements';
 import React from 'react';
 
-const AchievementContainer = () => {
-  const containerStyle = {
-    width: '1164px',
-    height: '848.75px',
-    flexShrink: 0,
-  };
-
-  return <div style={containerStyle}></div>;
+export const AchievementBox = () => {
+  return (
+    <div className='flex flex-col justify-center py-10 px-10 bg-blue-600 w-[90%] lg:w-[88%] xl:w-[85%]'>
+      <div className='flex justify-between'>
+        {achievements.map((achievement) => {
+          return (
+            <div key={achievement.id} className='justify-between'>
+              <div className='justify-center'>{achievement.icon}</div>
+              <div>{achievement.title_content}</div>
+              <div className='justify-center'>{achievement.label}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
-
-export default AchievementContainer;
