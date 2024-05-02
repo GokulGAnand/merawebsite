b940cca1212361dbc7e25e7cec8589243eaa4912
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-05-02 14:51:00 +0530

    chore: add five star

--- src/data/testimonials.tsx
+++ src/data/testimonials.tsx
@@ -7,28 +7,28 @@ import Nighil from '~/svg/Nighil.svg';
 export const testimonials = [
   {
     icon: <Vyshak width={60} height={60} />,
-    rating: 2.5,
+    rating: 5,
     customer: 'Vyshak',
     comments:
       'Selling my car was super quick and easy. Got a great deal without any trouble!',
   },
   {
     icon: <Jamshad width={60} height={60} />,
-    rating: 4,
+    rating: 5,
     customer: 'Jamshad',
     comments:
       'The inspection at home was so convenient. Really happy with the honest service.',
   },
   {
     icon: <Derin width={60} height={60} />,
-    rating: 4,
+    rating: 5,
     customer: 'Derin',
     comments:
       'I received my payment the very next day! Completely satisfied with the fast process.',
   },
   {
     icon: <Nighil width={60} height={60} />,
-    rating: 4,
+    rating: 5,
     customer: 'Nighil',
     comments:
       'I wanted to sell my car before migrating to Canada. The team was very helpful from start to finish!',
