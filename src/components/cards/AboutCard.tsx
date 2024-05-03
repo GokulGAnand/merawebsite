5aa5195d9c975e4fa84fd8fda495c1c0a93e99c9
itssalini <salini@teamtechmedia.com>
2024-05-03 16:55:31 +0530

    style: add about us page styles

--- src/components/cards/AboutCard.tsx
+++ src/components/cards/AboutCard.tsx
@@ -7,35 +7,43 @@ export const AboutCard = () => {
   const mission = about.filter((item) => item.id === 2);
   const vision = about.filter((item) => item.id === 3);
 
- 
-
   return (
-    <div className='mt-20'>
+    <div className='mt-24 lg:mb-36 mr:44'>
       <div className=''>
-        <p className='text-left text-16 font-bold leading-5 font-dm-sans font-medium text-aboutHeader'>
-        • About the Company
+        <p className='text-left text-16 font-bold leading-5 font-dm-sans font-medium text-bannerChipText'>
+          • About the Company
         </p>
         <HeaderWithBranding
           isStart
           heading='Our Company Overview'
           subHeading=''
         />
-        <div className='font-dm-sans text-base leading-[21px]'>{Overview[0].content}</div>
-      </div>
-      <div>
-        <Divider/>
-      </div>
-      <div>
-      <div className='grid grid-cols-2'>
-        <div>
-        <div className='text-aboutHeader font-dm-sans font-medium text-base leading-[23.44px]'>{mission[0].title}</div>
-        <div className='font-dm-sans text-base leading-[21px]'>{mission[0].content}</div>
-        </div>
-        <div>
-        <div className='text-aboutHeader font-dm-sans font-medium text-base leading-[23.44px]'>{vision[0].title}</div>
-        <div className='font-dm-sans text-base leading-[21px]'>{vision[0].content}</div>
+        <div className='font-dm-sans text-base text-sm leading-[21px]'>
+          {Overview[0].content}
         </div>
       </div>
+      <div>
+        <Divider />
+      </div>
+      <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2'>
+          <div>
+            <div className='text-bannerChipText font-dm-sans text-lg text-base leading-[23.44px]'>
+              {mission[0].title}
+            </div>
+            <div className='font-dm-sans text-base text-sm leading-[21px]'>
+              {mission[0].content}
+            </div>
+          </div>
+          <div>
+            <div className='text-bannerChipText font-dm-sans text-lg text-base leading-[23.44px]'>
+              {vision[0].title}
+            </div>
+            <div className='font-dm-sans text-base text-sm leading-[21px]'>
+              {vision[0].content}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/strips/AboutCompany.tsx
+++ src/components/strips/AboutCompany.tsx
@@ -9,13 +9,13 @@ export const AboutCompany = () => {
   return (
     <div className='flex justify-center'>
       <div className='flex-col w-[90%] lg:w-[88%] xl:w-[85%] justify-center'>
-        <div className='grid grid-cols-2 gap-2'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-2'>
           <AboutCard />
-          <div className='grid grid-cols-2'>
-            <div>
+          <div className='grid grid-cols-2 md:pl-20 lg:pl-16 xl:pl-44 2xl:pl-60'>
+            <div className='mt-[51px] lg:mt-20 pr-1.5 lg:pr-3 xl:pr-3 2xl-pr-3'>
               <Image src={about3} alt='About Image 1' />
             </div>
-            <div>
+            <div className='mt-[87px] lg:mt-[137px] mb-12'>
               <Image src={about4} alt='About Image 2' />
             </div>
           </div>
