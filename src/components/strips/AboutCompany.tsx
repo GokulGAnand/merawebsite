722de84f487f5a90682faca8a8b24c6c5e66d46d
itssalini <salini@teamtechmedia.com>
2024-05-06 09:11:29 +0530

    fix: about us page style fix

--- src/components/strips/AboutCompany.tsx
+++ src/components/strips/AboutCompany.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import Image from 'next/image';
-import about3 from '~/images/AboutUs/about-3.png';
-import about4 from '~/images/AboutUs/about-4.png';
 import { AboutCard } from '../cards/AboutCard';
+import { AboutImageCard } from '../cards/AboutImageCard';
 
 export const AboutCompany = () => {
   return (
     <div className='flex justify-center'>
-      <div className='flex-col w-[90%] lg:w-[88%] xl:w-[85%] justify-center'>
-        <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-2'>
+      <div className='flex flex-col w-[90%] lg:w-[88%] xl:w-[85%] justify-center'>
+        <div className='flex flex-col lg:flex-row lg:justify-between'>
           <AboutCard />
-          <div className='grid grid-cols-2 md:pl-20 lg:pl-16 xl:pl-44 2xl:pl-60'>
-            <div className='mt-[51px] lg:mt-20 pr-1.5 lg:pr-3 xl:pr-3 2xl-pr-3'>
-              <Image src={about3} alt='About Image 1' />
-            </div>
-            <div className='mt-[87px] lg:mt-[137px] mb-12'>
-              <Image src={about4} alt='About Image 2' />
-            </div>
-          </div>
+          <AboutImageCard />
         </div>
       </div>
     </div>
--- src/components/strips/Achievements.tsx
+++ src/components/strips/Achievements.tsx
@@ -8,14 +8,12 @@ export const AchievementBox = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center'>
           {achievements.map((achievement) => {
             return (
-              <div key={achievement.id} className='grid justify-items-center'>
-                <div className='mt-6 lg:mt-14 justify-center'>
-                  {achievement.icon}
-                </div>
-                <div className='font-dm-sans text-4xl font-normal leading-[62.5px] text-center text-white'>
+              <div key={achievement.id} className='flex flex-col items-center'>
+                <div className='mt-6 lg:mt-14'>{achievement.icon}</div>
+                <div className='text-4xl leading-[62.5px] text-white'>
                   {achievement.title_content}
                 </div>
-                <div className='mb-11 lg:mb-14  justify-center font-dm-sans text-base font-medium leading-[22.8px] text-center text-white'>
+                <div className='mb-11 lg:mb-14 text-base text-white'>
                   {achievement.label}
                 </div>
               </div>
