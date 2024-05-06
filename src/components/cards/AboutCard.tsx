7dff366e6b4ef2c6ea5f14726047edb0b9081020
itssalini <salini@teamtechmedia.com>
2024-05-06 09:11:29 +0530

    fix: about us page style fix

--- src/components/cards/AboutCard.tsx
+++ src/components/cards/AboutCard.tsx
@@ -1,16 +1,14 @@
-import { about } from '@/data/about';
+import { about, mission } from '@/data/about';
 import HeaderWithBranding from '../headers/HeaderWithBranding';
 import Divider from '../utils/Divider';
 
 export const AboutCard = () => {
-  const Overview = about.filter((item) => item.id === 1);
-  const mission = about.filter((item) => item.id === 2);
-  const vision = about.filter((item) => item.id === 3);
+  const Overview = about;
 
   return (
-    <div className='mt-24 lg:mb-36 mr:44'>
-      <div className=''>
-        <p className='text-left text-16 font-bold leading-5 font-dm-sans font-medium text-bannerChipText'>
+    <div className='mt-24 lg:mb-36 mr:44 lg:w-2/5'>
+      <div>
+        <p className='text-left text-base font-bold text-bannerChipText'>
           • About the Company
         </p>
         <HeaderWithBranding
@@ -18,31 +16,23 @@ export const AboutCard = () => {
           heading='Our Company Overview'
           subHeading=''
         />
-        <div className='font-dm-sans text-base text-sm leading-[21px]'>
-          {Overview[0].content}
-        </div>
+        <div className='text-sm leading-[21px]'>{Overview.content}</div>
       </div>
       <div>
         <Divider />
       </div>
-      <div>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2'>
-          <div>
-            <div className='text-bannerChipText font-dm-sans text-lg text-base leading-[23.44px]'>
-              {mission[0].title}
-            </div>
-            <div className='font-dm-sans text-base text-sm leading-[21px]'>
-              {mission[0].content}
-            </div>
-          </div>
-          <div>
-            <div className='text-bannerChipText font-dm-sans text-lg text-base leading-[23.44px]'>
-              {vision[0].title}
-            </div>
-            <div className='font-dm-sans text-base text-sm leading-[21px]'>
-              {vision[0].content}
-            </div>
-          </div>
+      <div className='flex'>
+        <div className='flex flex-col md:flex-row md:gap-x-8 gap-y-4'>
+          {mission.map((item) => {
+            return (
+              <div key={item.id}>
+                <div className='text-bannerChipText text-lg font-medium'>
+                  {item.title}
+                </div>
+                <div className='text-sm'>{item.content}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/data/about.tsx
+++ src/data/about.tsx
@@ -1,17 +1,17 @@
-export const about = [
+export const about = {
+  title: '',
+  content: `At Mera cars, we're passionate about connecting buyers and sellers  in the automotive marketplace. Whether you're looking to sell your
+        Whether you're looking to sell your`,
+};
+
+export const mission = [
   {
     id: 1,
-    title: '',
-    content: `At Mera cars, we're passionate about connecting buyers and sellers  in the automotive marketplace. Whether you're looking to sell your
-        Whether you're looking to sell your`,
-  },
-  {
-    id: 2,
     title: 'Our Mission',
     content: `At Mera cars, we're passionate about connecting buyers and sellers  in the automotive `,
   },
   {
-    id: 3,
+    id: 2,
     title: 'Our Vision',
     content: `At Mera cars, we're passionate about connecting buyers and sellers  in the automotive `,
   },
