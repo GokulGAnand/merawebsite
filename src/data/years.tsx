50f71f3c9d81dafc160868f884a795b81d1cec0c
akananthu12 <ananthakrishnan@teamtechmedia.com>
2024-05-22 10:05:41 +0530

    feat: add more years

--- src/data/years.tsx
+++ src/data/years.tsx
@@ -75,4 +75,20 @@ export const years = [
     label: '2006',
     value: '2006',
   },
+  {
+    label: '2005',
+    value: '2005',
+  },
+  {
+    label: '2004',
+    value: '2004',
+  },
+  {
+    label: '2003',
+    value: '2003',
+  },
+  {
+    label: '2002',
+    value: '2002',
+  },
 ];
