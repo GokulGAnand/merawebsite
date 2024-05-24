a1c91d5871d059f1d2a90bd135e534ec01e2977a
akanandhu <akananthu11@gmail.com>
2024-05-24 17:53:53 +0530

    style: responsiveness in car sell page

--- src/components/form/car/VehicleForm.tsx
+++ src/components/form/car/VehicleForm.tsx
@@ -16,7 +16,7 @@ export default function VehicleForm() {
   return (
     <div className='flex justify-center lg:px-0 px-4'>
       <div className='flex flex-col lg:w-3/4 w-full justify-center shadow-xl '>
-        <div className='bg-formBg w-full rounded-t-lg px-8 pt-8'>
+        <div className='bg-formBg w-full rounded-t-lg px-4 lg:px-8 lg:pt-8 pt-4'>
           <SideHeading heading='Enter your car registration number' />
           <SearchBar search={search} setSearch={setSearch} />
         </div>
@@ -24,7 +24,7 @@ export default function VehicleForm() {
           <div className='flex'>
             <OrSection />
           </div>
-          <div className='px-8'>
+          <div className='px-4 lg:px-8'>
             <SideHeading heading='Start with your car brand' />
             <div className='mt-4 mb-8'>
               <BrandOptions search={search} />
