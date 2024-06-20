4263ae94dfd2e13baa896dc4a3b6caa63810403f
akanandhu <akananthu11@gmail.com>
2024-06-20 14:45:20 +0530

    style: sidebar close on navigation

--- src/components/links/SidebarLinks.tsx
+++ src/components/links/SidebarLinks.tsx
@@ -6,6 +6,14 @@ import React from 'react';
 
 export const SidebarLinks = () => {
   const path = usePathname();
+  const handleLinkClick = () => {
+    const drawerCheckbox = document.getElementById(
+      'my-drawer',
+    ) as HTMLInputElement;
+    if (drawerCheckbox) {
+      drawerCheckbox.checked = false;
+    }
+  };
   return (
     <div>
       {headerLinks.map((link) => {
@@ -13,6 +21,7 @@ export const SidebarLinks = () => {
         return (
           <Link
             href={link.route}
+            onClick={handleLinkClick}
             key={link.label}
             className={`px-5 flex flex-col py-4 border border-solid border-b-mobileSidebarBorder border-x-0 border-t-0  text-${
               isActive ? 'bannerChipText' : `headerLinkColor`
