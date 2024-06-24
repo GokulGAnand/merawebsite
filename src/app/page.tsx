9e817379742b75e5e0a2916ce9a1f06c2d82bac1
akanandhu <ananthakrishnan@teamtechmedia.com>
2024-06-24 16:21:36 +0530

    chore: add scripts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/next-script-for-ga */
 import Head from 'next/head';
 import * as React from 'react';
 import '@/lib/env';
@@ -26,6 +29,41 @@ export default function HomePage() {
     <main className='overflow-x-clip'>
       <Head>
         <title>MeraCars - Home</title>
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-X6N59WN50T'
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-X6N59WN50T');
+          `}
+        </script>
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s) {
+              if(f.fbq) return; n=f.fbq=function() {
+                n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+              };
+              if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0';
+              n.queue=[]; t=b.createElement(e); t.async=!0;
+              t.src=v; s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '827600532663696');
+            fbq('track', 'PageView');
+          `}
+        </script>
+        <noscript>
+          <img
+            height='1'
+            width='1'
+            style={{ display: 'none' }}
+            src='https://www.facebook.com/tr?id=827600532663696&ev=PageView&noscript=1'
+          />
+        </noscript>
       </Head>
       <section>
         <MainBanner />
