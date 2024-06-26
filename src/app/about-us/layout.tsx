24e79af6ceef88bfd5e99a3299f807704a40f779
itssalini <salini@teamtechmedia.com>
2024-06-26 15:31:11 +0530

    chore: add pagebanner to about page from layout

--- src/app/about-us/layout.tsx
+++ src/app/about-us/layout.tsx
@@ -1,10 +1,7 @@
 import { Metadata } from 'next';
 import * as React from 'react';
 import '@/styles/colors.css';
-import PageBanner from '@/components/banner/common/PageBanner';
 import SectionBlock from '@/components/SectionBlock';
-import Banner from 'public/images/AboutUs/AboutBanner.png';
-import AboutBannerMobile from 'public/images/AboutUs/AboutBannerMobile.png';
 
 export const metadata: Metadata = {
   title: 'About us',
@@ -18,15 +15,7 @@ export default function AboutUsLayout({
 }) {
   return (
     <>
-      <SectionBlock>
-        <PageBanner
-          heading='About us'
-          subHeading='Home/About us'
-          srcPath={Banner}
-          srcMobile={AboutBannerMobile}
-        />
-        {children}
-      </SectionBlock>
+      <SectionBlock>{children}</SectionBlock>
     </>
   );
 }
--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import * as React from 'react';
-
 import Faq from '@/components/strips/Faq';
 import { AchievementBox } from '@/components/strips/Achievements';
 import { AboutCompany } from '@/components/strips/AboutCompany';
+import PageBanner from '@/components/banner/common/PageBanner';
+import Banner from 'public/images/AboutUs/AboutBanner.png';
+import AboutBannerMobile from 'public/images/AboutUs/AboutBannerMobile.png';
 
 export default function AboutPage() {
   return (
@@ -12,6 +14,12 @@ export default function AboutPage() {
         <title>About us</title>
       </Head>
       <section>
+        <PageBanner
+          heading='About us'
+          subHeading='Home/About us'
+          srcPath={Banner}
+          srcMobile={AboutBannerMobile}
+        />
         <AboutCompany />
         <AchievementBox />
         <Faq />
