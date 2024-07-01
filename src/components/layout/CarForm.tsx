fb42b6268d04a9c9d2e038137a7559e088cec4aa
itssalini <salini@teamtechmedia.com>
2024-07-01 14:34:47 +0530

    feat(inspection and sell your car): add vector on all slides

--- src/components/layout/CarForm.tsx
+++ src/components/layout/CarForm.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import UpVector from '~/images/SellUsedCars/VectorUp.svg';
+import DownVector from '~/images/SellUsedCars/VectorDown.svg';
 
 export default function CarFormLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main className='bg-carFormGradient'>{children}</main>;
+  return (
+    <div className='bg-carFormGradient'>
+      <div className='relative overflow-x-clip'>
+        <div className='absolute -left-16 top-0 hidden lg:flex z-50'>
+          <UpVector height={410} width={870} />
+        </div>
+        <div className='relative z-10 mt-16'>{children}</div>
+        <div className='absolute -right-2 -bottom-24 hidden lg:flex z-50'>
+          <DownVector height={410} width={870} />
+        </div>
+      </div>
+    </div>
+  );
 }
